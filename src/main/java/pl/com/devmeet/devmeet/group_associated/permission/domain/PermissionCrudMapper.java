8518226322ab2122bfea050c0ae887dd6b9cdf4e
Kamil <ka.ptasinski@gmail.com>
2019-10-13 21:50:59 +0200

    permission crud mapper implementation

--- src/main/java/pl/com/devmeet/devmeet/group_associated/permission/domain/PermissionCrudMapper.java
+++ src/main/java/pl/com/devmeet/devmeet/group_associated/permission/domain/PermissionCrudMapper.java
@@ -1,12 +1,29 @@
 package pl.com.devmeet.devmeet.group_associated.permission.domain;
 
+import pl.com.devmeet.devmeet.group_associated.group.domain.GroupCrudFacade;
+import pl.com.devmeet.devmeet.member_associated.member.domain.MemberCrudFacade;
+
 class PermissionCrudMapper {
 
     public static PermissionDto map (PermissionEntity entity){
-        return null;
+        return new PermissionDto().builder()
+                .member(MemberCrudFacade.map(entity.getMember()))
+                .group(GroupCrudFacade.map(entity.getGroup()))
+                .possibleToVote(entity.isPossibleToVote())
+                .possibleToMessaging(entity.isPossibleToMessaging())
+                .possibleToChangeGroupName(entity.isPossibleToChangeGroupName())
+                .possibleToBanMember(entity.isPossibleToBanMember())
+                .build();
     }
 
     public static PermissionEntity map (PermissionDto dto){
-        return null;
+        return new PermissionEntity().builder()
+                .member(MemberCrudFacade.map(dto.getMember()))
+                .group(GroupCrudFacade.map(dto.getGroup()))
+                .possibleToVote(dto.isPossibleToVote())
+                .possibleToMessaging(dto.isPossibleToMessaging())
+                .possibleToChangeGroupName(dto.isPossibleToChangeGroupName())
+                .possibleToBanMember(dto.isPossibleToBanMember())
+                .build();
     }
 }
