47c766c8a3c549e032e41ae3f6640d4b3b7b8dc1
Kamil <ka.ptasinski@gmail.com>
2019-10-19 13:00:59 +0200

    PermissionCrudFacade done, but there are problems with test member creation

--- src/main/java/pl/com/devmeet/devmeet/group_associated/permission/domain/PermissionCrudDeleter.java
+++ src/main/java/pl/com/devmeet/devmeet/group_associated/permission/domain/PermissionCrudDeleter.java
@@ -3,8 +3,11 @@ package pl.com.devmeet.devmeet.group_associated.permission.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
 import pl.com.devmeet.devmeet.domain_utils.CrudEntityDeleter;
+import pl.com.devmeet.devmeet.domain_utils.EntityAlreadyExistsException;
 import pl.com.devmeet.devmeet.domain_utils.EntityNotFoundException;
+import pl.com.devmeet.devmeet.group_associated.permission.domain.status_and_exceptions.PermissionCrudStatusEnum;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,7 +18,17 @@ class PermissionCrudDeleter implements CrudEntityDeleter<PermissionDto, Permissi
     private PermissionCrudSaver permissionCrudSaver;
 
     @Override
-    public PermissionEntity deleteEntity(PermissionDto dto) throws EntityNotFoundException {
-        return null;
+    public PermissionEntity deleteEntity(PermissionDto dto) throws EntityNotFoundException, EntityAlreadyExistsException {
+        PermissionEntity permissionEntity = permissionCrudFinder.findEntity(dto);
+        boolean permissionActivity = permissionEntity.isActive();
+
+        if (permissionActivity) {
+            permissionEntity.setActive(false);
+            permissionEntity.setModificationTime(DateTime.now());
+
+            return permissionCrudSaver.saveEntity(permissionEntity);
+        }
+
+        throw new EntityAlreadyExistsException(PermissionCrudStatusEnum.PERMISSION_FOUND_BUT_NOT_ACTIVE.toString());
     }
 }
