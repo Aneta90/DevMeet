8ce90fef7043a064ff0db5c0f79d5b4c96c2e279
Kamil <ka.ptasinski@gmail.com>
2019-10-27 21:04:35 +0100

    timestamp: before merge

--- src/main/java/pl/com/devmeet/devmeet/poll_associated/availability_vote/domain/AvailabilityVoteCrudFacade.java
+++ src/main/java/pl/com/devmeet/devmeet/poll_associated/availability_vote/domain/AvailabilityVoteCrudFacade.java
@@ -1,16 +1,60 @@
 package pl.com.devmeet.devmeet.poll_associated.availability_vote.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.com.devmeet.devmeet.domain_utils.CrudInterface;
 import pl.com.devmeet.devmeet.domain_utils.EntityAlreadyExistsException;
 import pl.com.devmeet.devmeet.domain_utils.EntityNotFoundException;
+import pl.com.devmeet.devmeet.member_associated.availability.domain.AvailabilityCrudRepository;
+import pl.com.devmeet.devmeet.member_associated.member.domain.MemberRepository;
+import pl.com.devmeet.devmeet.poll_associated.poll.domain.PollCrudRepository;
 
 import java.util.List;
 
 @Service
 public class AvailabilityVoteCrudFacade implements CrudInterface<AvailabilityVoteDto, AvailabilityVoteEntity> {
 
+    private AvailabilityVoteCrudRepository availabilityVoteRepository;
+    private PollCrudRepository pollCrudRepository;
+    private MemberRepository memberRepository;
+    private AvailabilityCrudRepository availabilityRepository;
 
+    @Autowired
+    public AvailabilityVoteCrudFacade(AvailabilityVoteCrudRepository availabilityVoteRepository,
+                                      PollCrudRepository pollCrudRepository,
+                                      MemberRepository memberRepository,
+                                      AvailabilityCrudRepository availabilityRepository) {
+
+        this.availabilityVoteRepository = availabilityVoteRepository;
+        this.pollCrudRepository = pollCrudRepository;
+        this.memberRepository = memberRepository;
+        this.availabilityRepository = availabilityRepository;
+    }
+
+    private AvailabilityVotePollFinder initPollFinder() {
+        return null;
+    }
+
+    private AvailabilityVoteMemberFinder initMemberFinder() {
+        return null;
+    }
+
+    private AvailabilityVoteAvailabilityFinder initAvailabilityFinder() {
+        return null;
+    }
+
+    private AvailabilityVoteCrudSaver initVoteSaver(){
+        return AvailabilityVoteCrudSaver.builder()
+                .availabilityVoteCrudRepository(availabilityVoteRepository)
+                .pollFinder(initPollFinder())
+                .memberFinder(initMemberFinder())
+                .availabilityFinder(initAvailabilityFinder())
+                .build();
+    }
+
+    private AvailabilityVoteCrudFinder initVoteFinder (){
+        return null;
+    }
 
     @Override
     public AvailabilityVoteDto create(AvailabilityVoteDto dto) throws EntityNotFoundException, EntityAlreadyExistsException {
