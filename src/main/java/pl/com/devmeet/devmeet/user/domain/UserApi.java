61f1f7242cf6e3ce3366962ba520beb6406b205d
Klaudia <khturowska@gmail.com>
2019-10-30 21:59:09 +0100

    added @CrossOrigin annotation to UserApi

--- src/main/java/pl/com/devmeet/devmeet/user/domain/UserApi.java
+++ src/main/java/pl/com/devmeet/devmeet/user/domain/UserApi.java
@@ -1,12 +1,14 @@
 package pl.com.devmeet.devmeet.user.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 class UserApi {
