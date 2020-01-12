0a24c65dc2a7e6c1d5942b5e6f4915c65d17231e
Aneta <aw22@onet.pl>
2020-01-12 22:06:24 +0100

    'PlaceVoteDto-added'

--- src/main/java/pl/com/devmeet/devmeetcore/poll_associated/place_vote/domain/PlaceVoteDto.java
+++ src/main/java/pl/com/devmeet/devmeetcore/poll_associated/place_vote/domain/PlaceVoteDto.java
@@ -1,4 +1,21 @@
 package pl.com.devmeet.devmeetcore.poll_associated.place_vote.domain;
 
+import lombok.*;
+import org.joda.time.DateTime;
+import pl.com.devmeet.devmeetcore.member_associated.member.domain.MemberDto;
+import pl.com.devmeet.devmeetcore.member_associated.place.domain.PlaceDto;
+import pl.com.devmeet.devmeetcore.poll_associated.poll.domain.PollDto;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class PlaceVoteDto {
+
+    private PollDto poll;
+    private PlaceDto place;
+    private MemberDto member;
+    private DateTime creationTime;
+    private boolean isActive;
 }
